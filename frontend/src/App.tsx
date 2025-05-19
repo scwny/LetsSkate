237ec8172eaa148b7eafc1a74fa57e5bca908e86
scwny <president@scwny.org>
2025-05-19 12:59:28 -0400

    feat: configure routing and protected route in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import { Hello } from './components/Hello';
-import './index.css';
+import { Login } from './pages/Login';
+import { PrivateRoute } from './components/PrivateRoute';
 
-function App() {
-  return (
-    <main className="p-6">
-      <Hello />
-    </main>
-  );
+function ProtectedPage() {
+  return <div className="p-6">🔒 This is protected content!</div>;
 }
 
-export default App;
+export default function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <nav className="p-4 bg-gray-100 space-x-4">
+          <Link to="/">Home</Link>
+          <Link to="/protected">Protected</Link>
+          <Link to="/login">Login</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Hello />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/protected"
+            element={
+              <PrivateRoute>
+                <ProtectedPage />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
