903798e6fd01350f1a3c5adb65eeece26baa7e96
scwny <president@scwny.org>
2025-05-19 12:00:25 -0400

    chore: setup React Query client provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';  // <-- make sure this line is present
+import './index.css';
+
+// 1. Import React Query
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// 2. Create a client
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* 3. Wrap your app */}
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
